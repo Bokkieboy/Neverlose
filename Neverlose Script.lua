269881c0714679e1621a037200ba0bc6e6ea5e7f
Bokkieboy <rhysfpounder@gmail.com>
2021-07-03 19:09:16 +0100

    Added trash talk and a watermark.

--- Neverlose Script.lua
+++ Neverlose Script.lua
@@ -5,11 +5,24 @@ local ESwitch = menu.Switch("Anti-Aim","Legit AA", false)
 local SafeMax = menu.Combo("Anti-Aim", "Legit AA", {"Safe", "Max", "Middle"}, 0, "Safe always works, Max is Risky but OP")
 local Jitter = menu.Switch("Anti-Aim", "Jitter", false)
 local JitterAngle = menu.SliderInt("Anti-Aim", "Jitter Angle", 0, -58 ,58)
-local TrashTalk = menu.Switch("Misc", "Trash Talk", false)
+local TrashTalk = menu.Switch("Misc", "Trash phrases", false)
 local Jitter1 = menu.Slider("Anti-Aim", "First Jitter", 0, -58, 58)
 local Jitter2 = menu.Slider("Anti-Aim", "Second Jitter", 0, -58, 58)
 local LBYJitter1 = menu.Slider("Anti-Aim", "First LBY Jitter", 0, -180, 180)
 local LBYJitter2 = menu.Slider("Anti-Aim", "Second LBY Jitter", 0, -180, 180)
+local Watermark = menu.Switch("Misc", "Watermark", true)
+
+local watermark=function()
+    local username = cheat.GetCheatUserName()
+    local text = "Bokkie Script | " .. username
+    local textSize = g_Render:CalcTextSize(text, 12)
+    local position = Vector2.new(20, 15)
+    local indent = Vector2.new(10, 5)
+    cheat.RegisterCallback("draw", function()
+        g_Render:BoxFilled(Vector2.new(position.x - indent.x, position.y - indent.y), Vector2.new(position.x + textSize.x + indent.x, position.y + textSize.y + indent.y), Color.new(0, 0, 0, 0.7))
+        g_Render:Text(text, Vector2.new(position.x, position.y), Color.new(1, 1, 1, 1), 12)
+    end)
+end
 
 function JitterOffset()
     if (RotationJitter == 10) then
@@ -18,12 +31,13 @@ function JitterOffset()
     elseif (RotationJitter == 11) then
         antiaim.OverrideLBYOffset(Jitter2:GetValue())
         RotationJitter = 10
-        end
+    end
 end
 
 JitterOff.RegisterCallback(JitterOffset)
 
-local Talk= {
+--Talk shit get ripped
+local phrases= {
     "Fire Burns",
     "Get sent to the Psych Ward",
     "Ez Media",
@@ -32,11 +46,24 @@ local Talk= {
     "Jordan's a pedo",
     "Springs fakes media"
 }
-local TalkGrab=function()
-    return Talk[math.random(1, #Talk):gsub('\"', '')]
+local get_phrase=function()
+    return phrases[math.random(1, #phrases):gsub('\"', '')]
 end
 
+if TrashTalk == true then
+    cheat.RegisterCallback("events", function(event)
 
+        if event:GetName() ~= "player_death" then return end
+
+        local me = g_EngineClient:GetLocalPlayer()
+       local victim = g_EngineClient:GetPlayerForUserId(event:GetInt("userid"))
+       local attacker = g_EngineClient:GetPlayerForUserId(event:GetInt("attacker"))
+
+       if victim == attacker or attacker ~= me then return end
+
+       g_EngineClient:ExecuteClientCmd('say "' .. get_phrase() .. '"')
+    end)
+end
 
 local JitterFunc=function()
     local jitteron=jitter:GetBool()
@@ -51,7 +78,6 @@ end
 local FakeLag=function()
     local FakeLagSwtichONOFF = FakeLagSwitch:GetBool()
     FakeLagRandomSlider:SetVisible(FakeLagSwitchONOFF)
-
 end
 
 FakeLag()
@@ -75,4 +101,4 @@ local LegitAA=function()
     end
 end
 LegitAA()
-ESwtichcheat.RegisterCallback(LegitAA) end
+ESwtichcheat.RegisterCallback(LegitAA)end
